247fc8ce2ca85bd268e66e0c6c146d5dfaeb6c20
faithmwangi <jon.codewj.dev@gmail.com>
2021-05-05 22:09:07 +0300

    m

---
+++ me.js
@@ -0,0 +1,19 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', hide);
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+    mainMenu.style.transition ='top 1s ease' ;
+
+}
+
+function hide() {
+    mainMenu.style.top = '-100%';
+}
+var d= new Date()
+document.getElementById("year").innerHTML = d.getFullYear();
