ca4bf4b8f6671865bb818c8ad817d16e7c7b723f
devcodewj <81858051+devcodewj@users.noreply.github.com>
2021-05-06 22:20:34 +0300

    Update me.js

--- me.js
+++ me.js
@@ -13,7 +13,7 @@ function show() {
 }
 
 function hide() {
-    mainMenu.style.top = '-150%';
+    mainMenu.style.top = '-300vh';
 }
 var d= new Date()
 document.getElementById("year").innerHTML = d.getFullYear();
